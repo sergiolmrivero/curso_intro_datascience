69bb4d6686e9c681868114dfd5fcb2622d19df06
Sergio Rivero <sergiolmrivero@gmail.com>
2019-03-13 12:14:42 -0300

    Atualizando a Apostila para UFPA

---
+++ curso_intro_datascience/Apostilas/sweaveBP.sh
@@ -0,0 +1,3 @@
+R CMD Sweave --encoding=utf8 BP_Curso_TecComp_00_2019.Rnw
+
+pdflatex BP_Curso_TecComp_00_2019.tex
---
+++ curso_intro_datascience/Apostilas/sweaveUfpa.sh
@@ -0,0 +1,3 @@
+R CMD Sweave --encoding=utf8 UFPA_Curso_TecComp_00_2019.Rnw
+
+pdflatex UFPA_Curso_TecComp_00_2019.tex
