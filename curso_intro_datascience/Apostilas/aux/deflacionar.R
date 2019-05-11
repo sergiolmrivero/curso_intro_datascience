65af729f54f3b3652fb515586e899327a3c65c83
Sergio Rivero <sergiolmrivero@gmail.com>
2019-05-10 22:05:12 -0300

    atualizando capitulo 7(6)

---
+++ curso_intro_datascience/Apostilas/aux/deflacionar.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(ggthemes)
+library(forecast)
+
+ggthemes::theme_economist()
+
+data <- read.csv2('data.csv', header=T, sep=';', dec=',')
+data$date <- as.Date(data$date, format='%d/%m/%Y')
+
+tail(data)
+
+ggplot(data, aes(date, previdencia/1000))+geom_line(size=.8)+
+  xlab('')+ylab('R$ bilhoes')+
+  labs(title='Despesa com Beneficios Previdenciarios')
+
+data$prevreal <- data[,3]*(tail(data[,2],1)/data[,2]) 
+
+ggplot(data, aes(date, previdencia/1000))+
+  geom_line(size=.8, aes(color='Serie Nominal'))+
+  geom_line(size=.8, aes(date, prevreal/1000, color='Serie Real'))+
+  xlab('')+ylab('R$ bilhoes')+
+  labs(title='Despesa com Beneficios Previdenciarios',
+       colour='',
+       caption='analisemacro.com.br')+
+  theme(legend.position="top")
+
+data.ts <- ts(data[,-1], start=c(1997,01), freq=12)
+
+#cmav(data.ts[,2], ma=12)
